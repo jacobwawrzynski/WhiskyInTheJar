9870666d85b153bf48569c8a1773b2f2f460ab81
Jakub Wawrzyński <jacobwawrzynski@gmail.com>
2023-12-28 02:16:07 +0100

    Add project files.

---
+++ Infrastructure/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=../Data.db");
+        }
+    }
+}
